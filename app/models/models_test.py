530f8fce97350c5e85d141012ed5ef778715c4ba
ANGELA <angela.mbithe@student.moringaschool.com>
2021-06-18 21:10:02 +0300

    add quote request

---
+++ app/models/models_test.py
@@ -0,0 +1,14 @@
+import unittest
+import quote
+Quote = quote.Quote
+
+class QuoteTest(unittest.TestCase):
+
+    def setUp (self):
+        self.new_quote = Quote(123,'i am wise', 'Angela Mutyota')
+
+    def test_instance(self):
+            self.assertTrue(isinstance(self.new_quote,Quote))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ app/models/quote.py
@@ -0,0 +1,10 @@
+class Quote:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self,id,quote,author):
+        self.id =id
+        self.quote = quote
+        self.author = author
+   
