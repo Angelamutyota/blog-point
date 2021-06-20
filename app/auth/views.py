9db9ca15fdb9ba3553c03d2ca6a3b47dcb79be2d
ANGELA <angela.mbithe@student.moringaschool.com>
2021-06-21 00:10:53 +0300

    add welcomr text

--- app/auth/views.py
+++ app/auth/views.py
@@ -18,6 +18,9 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to blog-point","email/welcome_user",user.email,user=user)
+
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
